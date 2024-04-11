1cd3104d1f8012dad3a22b341cf6a6dfe64bbf9f
Manoj Kumar Sen <112579743+Manoj-Sen@users.noreply.github.com>
2024-04-11 09:56:23 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Gray to Binary equivalent/gray-to-binary-equivalent.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution{
+    
+    // function to convert a given Gray equivalent n to Binary equivalent.
+    public static int grayToBinary(int n) {
+        
+        // Your code here
+        String grey = Integer.toBinaryString(n);
+        String bin = "" + grey.charAt(0);
+        for(int i=1;i<grey.length();i++)
+        {
+            if(bin.charAt(i-1)==grey.charAt(i)) bin+="0";
+            else bin+="1";
+        }
+        return Integer.parseInt(bin,2);
+        
+    }
+       
+}
+
+
+
+//{ Driver Code Starts.
+
+class GFG {
+
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();//testcases
+		while(t-->0){
+		    int n = sc.nextInt();//initializing n
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling grayToBinary() function
+		    System.out.println(obj.grayToBinary(n));
+		}
+	}
+}
+
+// } Driver Code Ends
