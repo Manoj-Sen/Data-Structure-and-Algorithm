76b99fdef126bc0c4376f50a43b90a263b375142
Manoj Kumar Sen <112579743+Manoj-Sen@users.noreply.github.com>
2024-05-08 21:47:36 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.java
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+   
+    // Function for finding maximum and value pair
+    public static int lenOfLongSubarr (int a[], int n, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        int maxl = 0;
+        int sum = 0;
+        
+        for(int i=0;i<n;i++){
+            sum+=a[i];
+            
+            if(sum==k){
+                maxl = Math.max(maxl,i+1);
+            }
+            
+            int r = sum-k;
+            if(map.containsKey(r)){
+                int len = i-map.get(r);
+                maxl = Math.max(maxl,len);
+            }
+            
+            if(!map.containsKey(sum)){
+                map.put(sum,i);
+            }
+        }
+        
+        return maxl;
+    }
+}
+
+
