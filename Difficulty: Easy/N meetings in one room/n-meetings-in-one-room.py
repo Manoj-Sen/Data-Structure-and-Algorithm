3c4a96a5815464bf88280fc51eafc357ba7cbef8
Manoj Kumar Sen <112579743+Manoj-Sen@users.noreply.github.com>
2024-08-08 19:34:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.py
@@ -0,0 +1,37 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find the maximum number of meetings that can
+    #be performed in a meeting room.
+    def maximumMeetings(self,n,start,end):
+        # code here
+        last=0
+        count=0
+        met=[(start[i],end[i]) for i in range(n)]
+        met.sort(key=lambda x:x[1])
+        for s,e in met:
+            if s>last:
+                last=e
+                count+=1
+        return count
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        start = list(map(int,input().strip().split()))
+        end = list(map(int,input().strip().split()))
+        ob=Solution()
+        print(ob.maximumMeetings(n,start,end))
+# } Driver Code Ends
