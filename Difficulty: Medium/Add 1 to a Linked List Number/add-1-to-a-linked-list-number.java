8bc8acfbd4a32750770608c89d0c7ee1ce11ca2e
Manoj Kumar Sen <112579743+Manoj-Sen@users.noreply.github.com>
2024-08-15 09:47:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+class GfG {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data);
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+            Node head = new Node(Integer.parseInt(str[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(str[i]));
+                tail = tail.next;
+            }
+            Solution obj = new Solution();
+            head = obj.addOne(head);
+            printList(head);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public Node rev(Node h){
+        Node curr=h,next=null,prev=null;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+    public Node addOne(Node head) {
+        // code here.
+        Node r=rev(head);
+        int carry=1;
+        Node curr=r,pre=null;
+        while(curr!=null){
+            curr.data+=carry;
+            carry=curr.data/10;
+            curr.data=curr.data%10;
+            pre=curr;
+            curr=curr.next;
+        }
+        if(carry>0) pre.next=new Node(carry);
+        return rev(r);
+    }
+}
