d5ea37aa4ad75a34efdb50d31e2ce6c257f53a72
Manoj Kumar Sen <112579743+Manoj-Sen@users.noreply.github.com>
2024-09-01 18:37:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max sum path in two arrays/max-sum-path-in-two-arrays.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maxPathSum(vector<int> &arr1, vector<int> &arr2) {
+        int sum1=0,sum2=0,ans=0;
+        int i=0,j=0;
+        while(i<arr1.size() && j<arr2.size())
+        {
+            if(arr1[i]==arr2[j])
+            {
+                sum1+=arr1[i];
+                sum2+=arr2[j];
+                ans+=max(sum1,sum2);
+                i++;j++;
+                sum1=0;sum2=0;
+            }
+            else if(arr1[i]>arr2[j])
+            {
+                sum2+=arr2[j];
+                j++;
+            }
+            else 
+            {
+                sum1+=arr1[i];
+                i++;
+            }
+        }
+        while(i<arr1.size())
+        {
+             sum1+=arr1[i];
+                i++;
+        }
+        while(j<arr2.size())
+        {
+            sum2+=arr2[j];
+                j++;
+        }
+        ans+=max(sum1,sum2);
+        return ans;
+       
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr1;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr1.push_back(number);
+        }
+        vector<int> arr2;
+        string input2;
+        getline(cin, input2);
+        stringstream ss2(input2);
+        int number2;
+        while (ss2 >> number2) {
+            arr2.push_back(number2);
+        }
+        Solution ob;
+        long long ans = ob.maxPathSum(arr1, arr2);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
