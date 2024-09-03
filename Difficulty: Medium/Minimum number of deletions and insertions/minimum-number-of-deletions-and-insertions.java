e0b143384202c3b6f8335238a466d3bed4e08a57
Manoj Kumar Sen <112579743+Manoj-Sen@users.noreply.github.com>
2024-09-03 21:38:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum number of deletions and insertions/minimum-number-of-deletions-and-insertions.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s1 = sc.next();
+                    String s2 = sc.next();
+                    Solution ob = new Solution();
+                    System.out.println(ob.minOperations(s1,s2));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+	public int minOperations(String str1, String str2) 
+	{ 
+	    // Your code goes here
+	    int[][] dp = new int[str1.length()+1][str2.length()+1];
+	    for(int i=1; i<dp.length; i++){
+	        for(int j=1; j<dp[0].length; j++){
+	            if(str1.charAt(i-1) == str2.charAt(j-1)){
+	                dp[i][j] = 1 + dp[i-1][j-1];
+	            }else{
+	                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+	            }
+	        }
+	    }
+	    
+	    int commonLen = dp[str1.length()][str2.length()];
+	    return (str1.length()-commonLen) + (str2.length()-commonLen);
+	} 
+}
