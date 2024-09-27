ebff283ce30b26170862e6fe3230d9083dab4dad
Manoj Kumar Sen <112579743+Manoj-Sen@users.noreply.github.com>
2024-09-27 18:37:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+// Initial template for JAVA
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        // taking input using class Scanner
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            // taking total number of elements
+            int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            ArrayList<Integer> res = new Solution().max_of_subarrays(k, arr);
+
+            // printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++) System.out.print(res.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function template for JAVA
+
+class Solution {
+    // Function to find maximum of each subarray of size k.
+    public ArrayList<Integer> max_of_subarrays(int k, int arr[]) {
+        // Your code here
+        Deque<Integer> dq=new ArrayDeque<>();
+        ArrayList<Integer> al=new ArrayList<>();
+        int n = arr.length;
+        int i;
+        for(i=0;i<k;++i){
+            while(!dq.isEmpty() && arr[i]>=arr[dq.peekLast()]){
+                dq.removeLast();
+            }
+            dq.addLast(i);
+        }
+        
+        for(;i<n;++i){
+            al.add(arr[dq.peek()]);
+            while(!dq.isEmpty() && dq.peek() <= i-k){
+                dq.removeFirst();
+            }
+            while(!dq.isEmpty() && arr[i]>=arr[dq.peekLast()]){
+                dq.removeLast();
+            }
+            dq.addLast(i);
+        }
+        al.add(arr[dq.peek()]);
+        
+        return al;
+    }
+}
