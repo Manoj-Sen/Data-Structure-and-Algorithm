47c5e246207735c6653764a14415710f9d400844
Manoj Kumar Sen <112579743+Manoj-Sen@users.noreply.github.com>
2024-09-30 14:05:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two BST 's/merge-two-bst-s.java
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.java
@@ -111,52 +111,17 @@ class Solution {
     // Function to return a list of integers denoting the node
     // values of both the BST in a sorted order.
     public List<Integer> merge(Node root1, Node root2) {
-        List<Integer> l1 = new ArrayList<>();
-        List<Integer> l2 = new ArrayList<>();
-        
-        inorder(root1, l1);
-        inorder(root2, l2);
-        
-        return mergeSortedLists(l1, l2);
+        List<Integer> sortedList = new ArrayList<>();
+        inOrderTraversal(root1, sortedList);
+        inOrderTraversal(root2, sortedList);
+        sortedList.sort(Integer::compareTo);
+        return sortedList;
     }
     
-
-    void inorder(Node root, List<Integer> list) {
+    private void inOrderTraversal(Node root, List<Integer> sortedList) {
         if (root == null) return;
-        
-        inorder(root.left, list);
-        list.add(root.data);
-        inorder(root.right, list);
+        inOrderTraversal(root.left, sortedList);
+        sortedList.add(root.data);
+        inOrderTraversal(root.right, sortedList);
     }
-    
-   
-    List<Integer> mergeSortedLists(List<Integer> l1, List<Integer> l2) {
-        List<Integer> mergedList = new ArrayList<>();
-        int i = 0, j = 0;
-        
-        while (i < l1.size() && j < l2.size()) {
-            if (l1.get(i) < l2.get(j)) {
-                mergedList.add(l1.get(i));
-                i++;
-            } else {
-                mergedList.add(l2.get(j));
-                j++;
-            }
-        }
-        
-    
-        while (i < l1.size()) {
-            mergedList.add(l1.get(i));
-            i++;
-        }
-        
-       
-        while (j < l2.size()) {
-            mergedList.add(l2.get(j));
-            j++;
-        }
-        
-        return mergedList;
-    }
-
 }
