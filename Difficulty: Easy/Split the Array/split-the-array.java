d5dd192f61a7787ae87730e312f2f779c2a93955
Manoj Kumar Sen <112579743+Manoj-Sen@users.noreply.github.com>
2024-10-21 10:01:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split the Array/split-the-array.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+
+     private static final int MOD = 1000000007;
+    public static int countgroup(int arr[]) {
+        // Complete the function
+        int totalXOR = 0;
+        for (int num : arr) {
+            totalXOR ^= num;
+        }
+        if (totalXOR != 0) {
+            return 0;
+        }
+        int n = arr.length;
+        long result = modPow(2, n - 1, MOD) - 1;
+        
+        if (result < 0) {
+            result += MOD;
+        }
+
+        return (int) result;
+    }
+    private static long modPow(long base, long exp, long mod) {
+        long result = 1;
+        while (exp > 0) {
+            if ((exp & 1) == 1) {
+                result = (result * base) % mod;
+            }
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+        return result;  
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            // int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int ans = obj.countgroup(arr);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
