86f0ed91d5f99a46eff9c20a50cf1b7898422895
Manoj Kumar Sen <112579743+Manoj-Sen@users.noreply.github.com>
2024-10-27 20:28:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Reverse a Linked List in Groups/reverse-a-linked-list-in-groups.java
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int key) {
+        data = key;
+        next = null;
+    }
+}
+
+class ReverseInSize {
+    static Node head;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+
+        while (t-- > 0) {
+
+            String s[] = in.readLine().trim().split(" ");
+            int a1 = Integer.parseInt(s[0]);
+            Node head = new Node(a1);
+            Node tail = head;
+            for (int i = 1; i < s.length; i++) {
+                int a = Integer.parseInt(s[i]);
+                // addToTheLast(new Node(a));
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+            int k = Integer.parseInt(in.readLine().trim());
+            Solution ob = new Solution();
+            Node res = ob.reverse(head, k);
+            printList(res, out);
+            out.println();
+
+            out.println("~");
+        }
+        out.close();
+    }
+
+    public static void printList(Node node, PrintWriter out) {
+        while (node != null) {
+            out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*node class of the linked list
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution {
+    public static Node reverse(Node node) {
+        Node curr = node, prev = null;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static Node reverse(Node node, int k) {
+        Node curr = node;
+        Node dummy = new Node(0);
+        Node prev = dummy;
+        
+        while (curr != null) {
+            Node temp = curr;
+            for (int i = 0; i < k - 1 && temp.next != null; i++) {
+                temp = temp.next;
+            }
+            
+            Node next = temp.next;
+            temp.next = null;
+            
+            Node rev = reverse(curr);
+            prev.next = rev;
+            
+            prev = curr;
+            curr = next;
+        }
+        
+        return dummy.next;
+    }
+}
